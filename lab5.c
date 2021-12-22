ca14256e14313f9181824e9c3fb92895090fcf3b
João Victor <77211641+Gadelhous@users.noreply.github.com>
2021-12-22 01:31:49 -0300

    Update lab5.c
    versão "0.9", aonde o código executa o que é pedido porém com um pequeno "twist" (funciona quando nthread = 6, não com 5 [provavelmente por conta de alguma lógica do switch])

--- lab5.c
+++ lab5.c
@@ -1,15 +1,15 @@
 /* Disciplina: Computacao Concorrente */
 /* Prof.: Silvana Rossetto */
-/* Laboratório: 4 */
+/* Laboratório: 5 */
 /* Codigo: Uso de variáveis de condição e suas operações básicas para sincronização por condição */
 
-/***** Condicao logica da aplicacao: a thread B so pode imprimir "Bye" depois que duas threads A imprimirem  "Hello"  ****/
+/***** Condicao logica da aplicacao: a thread 1 so pode imprimir após as threads 2,3 e 4, que só podem imprimir após a thread 5  ****/
 
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NTHREADS  5
+#define NTHREADS  6
 
 /* Variaveis globais, sendo X a variável de sincronização para as threads 2,3 e 4; e Y a variável de sincronização para a thread 1*/
 int x = 0;
@@ -22,75 +22,82 @@ pthread_cond_t y_cond;
 void *tarefa (void *arg){
    long long int id = (long long int) arg; //identificador da thread
     switch(id){
-       case 1:
-       if (y < 3) { 
-            printf("1: y= %d, vai se bloquear...\n", y);
-            pthread_cond_wait(&y_cond, &y_mutex);
-            printf("1: sinal recebido e mutex realocado, y = %d\n", y);
-        }
-        printf("Volte sempre!");
-        break;
+        case 1:
+            pthread_mutex_lock(&y_mutex);
+            if (y < 3) { 
+                
+                pthread_cond_wait(&y_cond, &y_mutex);
+                
+            }
+            printf("Volte sempre!\n");
+            pthread_mutex_unlock(&y_mutex);
+            break;
 
-       case 2:
-       if (x < 1) { 
-            printf("2: x= %d, vai se bloquear...\n", x);
-            pthread_cond_wait(&x_cond, &x_mutex);
-            printf("2: sinal recebido e mutex realocado, x = %d\n", x);
-        }
-        printf("Fique a vontade.");
-        pthread_mutex_lock(&y_mutex);
-        y++;
-        pthread_mutex_unlock(&y_mutex);
-        if (y==3) {
-            printf("2:  y = %d, vai sinalizar a condicao \n", y);
-            pthread_cond_signal(&y_cond);
-        }
-        pthread_exit(NULL);
-        break; 
+        case 2:
+            pthread_mutex_lock(&x_mutex);
+            if (x < 1) { 
+                
+                pthread_cond_wait(&x_cond, &x_mutex);
+               
+            }
+            printf("Fique a vontade.\n");
+            pthread_mutex_lock(&y_mutex);
+            y++;
+            pthread_mutex_unlock(&y_mutex);
+            if (y==3) {
+                
+                pthread_cond_signal(&y_cond);
+            }
+            pthread_mutex_unlock(&x_mutex);
+            break; 
 
-       case 3:
-       if (x < 1) { 
-            printf("3: x= %d, vai se bloquear...\n", x);
-            pthread_cond_wait(&x_cond, &x_mutex);
-            printf("3: sinal recebido e mutex realocado, x = %d\n", x);
-        }
-        printf("Sente-se por favor.");
-        pthread_mutex_lock(&y_mutex);
-        y++;
-        pthread_mutex_unlock(&y_mutex);
-        if (y==3) {
-        printf("3:  y = %d, vai sinalizar a condicao \n", y);
-        pthread_cond_signal(&y_cond);
-        }
-        pthread_mutex_unlock(&y_mutex);
-        pthread_exit(NULL);
-        break; 
+        case 3:
+            pthread_mutex_lock(&x_mutex);
+            if (x < 1) { 
+               
+                pthread_cond_wait(&x_cond, &x_mutex);
+                
+            }
+            printf("Sente-se por favor.\n");
+            pthread_mutex_lock(&y_mutex);
+            y++;
+            pthread_mutex_unlock(&y_mutex);
+            if (y==3) {
+                
+                pthread_cond_signal(&y_cond);
+            }
+            pthread_mutex_unlock(&x_mutex);
+            break; 
 
-       case 4:
-       if (x < 1) { 
-            printf("4: x= %d, vai se bloquear...\n", x);
-            pthread_cond_wait(&x_cond, &x_mutex);
-            printf("4: sinal recebido e mutex realocado, x = %d\n", x);
-        }
-        printf("Aceita um copo d’agua?.");
-        pthread_mutex_lock(&y_mutex);
-        y++;
-        pthread_mutex_unlock(&y_mutex);
-        if (y==3) {
-            printf("4:  y = %d, vai sinalizar a condicao \n", y);
-            pthread_cond_signal(&y_cond);
-        }
-       pthread_exit(NULL);
-       break; 
+        case 4:
+            pthread_mutex_lock(&x_mutex);
+            if (x < 1) { 
+                
+                pthread_cond_wait(&x_cond, &x_mutex);
+                
+            }
+            printf("Aceita um copo d’agua?.\n");
+            pthread_mutex_lock(&y_mutex);
+            y++;
+            pthread_mutex_unlock(&y_mutex);
+            if (y==3) {
+                
+                pthread_cond_signal(&y_cond);
+            };
+            pthread_mutex_unlock(&x_mutex);    
+             break; 
 
-       case 5:
-       printf("Seja bem-vindo!");
-       pthread_mutex_lock(&x_mutex);
-       x++;
-       pthread_mutex_unlock(&x_mutex);
-       pthread_cond_broadcast(&x_cond);
-       pthread_exit(NULL);
-       break; 
+        case 5:
+            printf("Seja bem-vindo!\n");
+            pthread_mutex_lock(&x_mutex);
+            x++;
+            pthread_mutex_unlock(&x_mutex);
+            pthread_cond_broadcast(&x_cond);
+            break; 
+
+        case 6:
+            printf("TESTE TESTE");
+    pthread_exit(NULL);
     }
 }
 
@@ -122,8 +129,7 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < NTHREADS; i++) {
     pthread_join(*(tid+i), NULL);
   }
-  printf ("\nFIM\n");
-
+ 
   /* Desaloca variaveis e termina */
   pthread_mutex_destroy(&x_mutex);
   pthread_cond_destroy(&x_cond);
