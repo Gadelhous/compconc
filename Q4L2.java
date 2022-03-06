955ee679ffee38bf54d516b7038b6bed8ed6231a
João Victor <77211641+Gadelhous@users.noreply.github.com>
2022-03-06 11:37:06 -0300

    Update Q4L2.java

--- Q4L2.java
+++ Q4L2.java
@@ -3,53 +3,50 @@ import java.util.concurrent.PriorityBlockingQueue;
 
 //Queue/PriorityQueue/PriorityBlockingQueue
 class LE {
-    private int leit, escr;  
-    
+    private int leit, escr,limite;
+
     // Construtor
-    LE() { 
-       this.leit = 0; //leitores lendo (0 ou mais)
-       this.escr = 0; //escritor escrevendo (0 ou 1)
-    } 
-    
-    // Entrada para leitores
-    public synchronized void EntraLeitor (int id) {
-      try { 
-        while (this.escr > 0) {
-        //if (this.escr > 0) {
-           System.out.println ("le.leitorBloqueado("+id+")");
-           wait();  //bloqueia pela condicao logica da aplicacao 
-        }
-        this.leit++;  //registra que ha mais um leitor lendo
-        System.out.println ("le.leitorLendo("+id+")");
-      } catch (InterruptedException e) { }
+    LE() {
+        this.leit = 0; //Impressora lendo (0 ou mais)
+        this.escr = 0; //Requisitora escrevendo (0 ou 1)
+        this.limite = 0;
     }
-    
+
+    // Entrada para impressora
+    public synchronized void EntraLeitor (int id) {
+        try {
+            while (limite == 0) {
+                System.out.println ("le.ImpressorBloqueado("+id+") : A fila de impressão está vazia!" );
+                wait();  //bloqueia pela condicao logica da aplicacao
+            }
+            this.leit++;  //registra que ha mais um leitor lendo
+            //System.out.println ("le.ImpressorLendo("+id+")");
+        } catch (InterruptedException e) { }
+    }
+
     // Saida para leitores
     public synchronized void SaiLeitor (int id) {
-       this.leit--; //registra que um leitor saiu
-       if (this.leit == 0) 
-             this.notify(); //libera escritor (caso exista escritor bloqueado)
-       System.out.println ("le.leitorSaindo("+id+")");
+        this.leit--; //registra que um leitor saiu
+        //System.out.println ("le.ImpressorSaindo("+id+")");
     }
-    
+
     // Entrada para escritores
     public synchronized void EntraEscritor (int id) {
-      try { 
-        while ((this.leit > 0) || (this.escr > 0)) {
-        //if ((this.leit > 0) || (this.escr > 0)) {
-           System.out.println ("le.escritorBloqueado("+id+")");
-           wait();  //bloqueia pela condicao logica da aplicacao 
-        }
-        this.escr++; //registra que ha um escritor escrevendo
-        System.out.println ("le.escritorEscrevendo("+id+")");
-      } catch (InterruptedException e) { }
+        try {
+            while ((limite > lab8.fila.size())) {
+                System.out.println ("le.requisitorBloqueado("+id+") : A fila está cheia!");
+                wait();  //bloqueia pela condicao logica da aplicacao
+            }
+            this.escr++; //registra que ha um escritor escrevendo
+            //System.out.println ("le.requisitorRequisitando("+id+")");
+        } catch (InterruptedException e) { e.printStackTrace(); }
     }
-    
+
     // Saida para escritores
     public synchronized void SaiEscritor (int id) {
-       this.escr--; //registra que o escritor saiu
-       notifyAll(); //libera leitores e escritores (caso existam leitores ou escritores bloqueados)
-       System.out.println ("le.escritorSaindo("+id+")");
+        this.escr--; //registra que o requisitor saiu
+        notify(); //libera leitores e escritores (caso existam leitores ou escritores bloqueados)
+        //System.out.println ("le.RequisitorSaindo("+id+")");
     }
 
 }
@@ -57,58 +54,39 @@ class LE {
 
 
 //Aplicacao de exemplo--------------------------------------------------------
-// Leitor
-class Leitor extends Thread {
-    int id; //identificador da thread
-    int delay; //atraso bobo
-    LE monitor;//objeto monitor para coordenar a lógica de execução das threads
-  
-    // Construtor
-    Leitor (int id, int delayTime, LE m) {
-        this.id = id;
-        this.delay = delayTime;
-        this.monitor = m;
-    }
-  
-    // Método executado pela thread
-    public void run () {
-        try {
-            for (;;) {
-                this.monitor.EntraLeitor(this.id);
-                lab8.ehPar();
-                this.monitor.SaiLeitor(this.id);
-                sleep(this.delay); 
-            }
-        } catch (InterruptedException e) { return; }
-    }
-}
-  
-//--------------------------------------------------------
-// Escritor
-class Escritor extends Thread {
+// Requisitora
+class Requisitora extends Thread {
     int id; //identificador da thread
     int delay; //atraso bobo...
     LE monitor; //objeto monitor para coordenar a lógica de execução das threads
 
-  
+
     // Construtor
-    Escritor (int id, int delayTime, LE m) {
-      this.id = id;
-      this.delay = delayTime;
-      this.monitor = m;
+    Requisitora(int id, int delayTime, LE m) {
+        this.id = id;
+        this.delay = delayTime;
+        this.monitor = m;
     }
-  
+
     // Método executado pela thread
     public void run () {
         try {
             for (;;) {
-                this.monitor.EntraEscritor(this.id); 
-                lab8.trocaVariavel(this.id);    
-                this.monitor.SaiEscritor(this.id); 
-                sleep(this.delay); 
+                this.monitor.EntraEscritor(this.id);
+                this.requisita(" x ");
+                this.monitor.SaiEscritor(this.id);
+                sleep(this.delay);
             }
-      } catch (InterruptedException e) { return; }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return; }
     }
+
+    public void requisita(String r){
+        System.out.println("Eu, a requisitora " +this.id+ " estou adicionando uma requisição na fila");
+        lab8.fila.add(r);
+    }
+
 }
 
 //--------------------------------------------------------
@@ -124,81 +102,64 @@ class Impressora extends Thread {
         this.delay = delayTime;
         this.monitor = m;
     }
-    
+
     // Método executado pela thread
     public void run () {
-        double j=777777777.7, i;
-        /*while(1){
 
-        }
-        */
         try {
-          for (;;) {
+            for (;;) {
                 this.monitor.EntraLeitor(this.id);
-
+                this.imprime();
                 this.monitor.SaiLeitor(this.id);
-                sleep(this.delay); 
-                for (i=0; i<100; i++) {j=j/2;} //...loop bobo para simbolizar o tempo de escrita
-                this.monitor.EntraEscritor(this.id); 
-                lab8.accVariavel();
-                this.monitor.SaiEscritor(this.id); 
-          }
-        } catch (InterruptedException e) { return; }
-      }
+                sleep(this.delay);
+            }
+        } catch (InterruptedException e) { e.printStackTrace(); }
+    }
+
+    private void imprime(){
+        System.out.println("\nEstou Imprimindo");
+        lab8.fila.poll();
+        notify();
+    }
 }
 
 //--------------------------------------------------------
 // Classe principal
 class lab8{
 
-    static final int L = 4;
-    static final int E = 3;
+    static final int E = 3; //E = Requisitora
     static final int LESC = 1;  //LESC = Impressora;
 
-    static int variavel = 0;
-  
+    static int tamanho_máximo= 5;
+    static int contador = 0;
 
-    PriorityBlockingQueue<Integer> pbq
-    = new PriorityBlockingQueue<Integer>();
 
-    public static void ehPar(){
-        if(variavel % 2 == 0){
-            System.out.println("O número é par");
-        }else{
-            System.out.println("O número é ímpar");
-        }
+    static PriorityBlockingQueue<String> fila
+            = new PriorityBlockingQueue<String>();
+
+
+
+    public void setFila(PriorityBlockingQueue<String> fila) {
+        this.fila = fila;
     }
 
-    public static void accVariavel(){
-        variavel++;
-    }
 
-    public static void trocaVariavel(int a){
-        variavel = a;
-    }
 
-    public static void printaVariavel(){
-        System.out.println(variavel);
-    }
     public static void main (String[] args) {
         int i;
         LE monitor = new LE();            // Monitor (objeto compartilhado entre leitores e escritores)
-        Leitor[] l = new Leitor[L];       // Threads leitores
-        Escritor[] e = new Escritor[E];   // Threads escritores
+        Requisitora[] e = new Requisitora[E];   // Threads escritores
         Impressora[] lEsc = new Impressora[LESC];       // Threads leitoras/escritoras
-    
-        //inicia o log de saida
+
+        /*inicia o log de saida
         System.out.println ("import verificaLE");
         System.out.println ("le = verificaLE.LE()");
-        
-        for (i=0; i<L; i++) {
-            l[i] = new Leitor(i+1, (i+1)*500, monitor);
-            l[i].start(); 
-        }
+        */
+
 
         for (i=0; i<E; i++) {
-            e[i] = new Escritor(i+1, (i+1)*500, monitor);
-            e[i].start(); 
+            e[i] = new Requisitora(i+1, (i+1)*500, monitor);
+            e[i].start();
         }
 
         for(i = 0; i<LESC; i++){
